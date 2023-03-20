5ebd10b908467c858592c7d663af2636a1907117
Manuel <siguiendotujuego@gmail.com>
2023-03-20 01:11:42 -0500

    05-secuencias-escape

---
+++ tipos/05-secuencias-escape.py
@@ -0,0 +1,26 @@
+curso = "Ultimate \"Python\""
+
+r"""
+se puede usar comillas simples para separar las commillas y
+ las comillas simples, pero tamben se puede usar \ que se usa para que la
+ litra despues de \ no se cuente como codigo 
+"""
+# al utilizar el prefijo "r" antes de una cadena de texto, se indica a Python
+#  que se trata de una "cadena cruda" o "raw string", lo que significa que
+# cualquier carácter especial dentro de la cadena se debe interpretar de forma
+#  literal, sin procesar secuencias de escape.
+
+# comentario
+# \" = permite uqe se imprima "
+# \' = permite uqe se imprima '
+# \\ = permite uqe se imprima \
+# \n = toma el sring que viene despues de \ y lo pasa a una nueva linea
+
+
+print(curso)
+
+cursi = "Ultimate 'Python'"
+print(cursi)
+
+cursi = "Ultimate \n\"Python\" "
+print(cursi)
